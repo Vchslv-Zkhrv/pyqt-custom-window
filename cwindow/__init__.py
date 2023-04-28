b6a5f02a4ccda7425fc2f58fbcabd20daabedbef
Vchslv-Zkhrv <vchslv.zkhrv@gmail.com>
2023-04-28 19:50:30 +0300

    all logic about gestures incapsulated in CWindow class

--- cwindow/__init__.py
+++ cwindow/__init__.py
@@ -1,4 +1,5 @@
 from . import grips
 from . import shadow
 from . import window
+from . import modes
 from .window import CWindow
--- cwindow/window.py
+++ cwindow/window.py
@@ -3,139 +3,7 @@ from PyQt6 import QtCore, QtGui, QtWidgets
 from .grips import SideGrip
 from .shadow import WindowShadow
 from .parsers import EventParser, ScreenParser
-
-
-class TitleBar(QtWidgets.QFrame):
-
-    """
-    Custom window title bar. Provides moving for frameless window
-    """
-
-    def __init__(
-            self,
-            parent: QtWidgets.QMainWindow,
-            height: int):
-
-        QtWidgets.QFrame.__init__(self, parent)
-        self.setFixedHeight(height)
-        self.setSizePolicy(
-            QtWidgets.QSizePolicy(
-                QtWidgets.QSizePolicy.Policy.Expanding,
-                QtWidgets.QSizePolicy.Policy.Fixed)
-            )
-
-        self._is_pressed = False
-        # _is_gestured indicates that window was resized with moving to screen edge gesture
-        self._is_gestured = False
-        self._shadow = WindowShadow(parent.shadow_color)
-        self.window_normal_size = parent.size()
-        self._screen = ScreenParser(self.screen())
-
-    def mousePressEvent(self, a0: QtGui.QMouseEvent) -> None:
-        self._is_pressed = True
-        self._press_event = EventParser(self, a0)
-        self.window().setCursor(QtCore.Qt.CursorShape.ClosedHandCursor)
-        return super().mousePressEvent(a0)
-
-    def mouseMoveEvent(self, a0: QtGui.QMouseEvent) -> None:
-        if self._is_pressed:
-            # if pressed, checks that user moved window to the screen edge 
-            # and shows the shadow to indicate target window geometry
-            parser = EventParser(self, a0)
-            if parser.side and parser.shadow_rect:
-                self._shadow.show_(parser.shadow_rect)
-            else:
-                self._shadow.hide()
-        return super().mouseMoveEvent(a0)
-
-    def _use_shadow_geometry(self):
-
-        """hides shadow and sets it's geometry to window"""
-
-        if not self._is_gestured:
-            # saves window old size to restore it later
-            self.window_normal_size = self.window().size()
-        self._shadow.hide()
-        self._is_gestured = True
-        self.window().setGeometry(self._shadow.geometry())
-
-    def _move_normal(self):
-
-        """common way to move the window"""
-
-        # calculates delta between pressed and released points
-        pos1 = self._release_event.point
-        pos0 = self._press_event.point
-        dx = pos1.x() - pos0.x()
-        dy = pos1.y() - pos0.y()
-        posW = self.window().pos()
-        self.window().move(dx + posW.x(), dy + posW.y())
-
-    def _fix_x_delta_after_resize(self):
-
-        """
-        makes screen edge gesture exiting more intuitive
-        """
-        # window position after moving and resizing
-        pos = self.window().pos()
-
-        # if window was moved right
-        if self._release_event.point.x() > self._press_event.point.x():
-            x = self._press_event.event.pos().x()
-            # delta x = window width * relative cursor position when it was pressed
-            x -= int(self.window().width() * self._press_event.relative_pos[0])
-            pos.setX(x)
-
-        # if window was moved left
-        else:
-            # up to this point, the _release_event characteristics
-            # have been computed relative to the old window geometry.
-            # Updates them
-            self._release_event.parse_event()
-            # rdx = relative delta x = difference between
-            # cursor relative position to the old window geometry and the new one
-            rdx = abs(self._press_event.relative_pos[0] - self._release_event.relative_pos[0])
-            # delta x = window width * rdx
-            dx = int(self.window().width()*rdx)
-            pos.setX(pos.x() + dx)
-
-        self.window().move(pos)
-
-    def _restore_normal_size(self):
-        """
-        Restores normal window size after gestures
-        """
-        width = self.window().width()
-        self.window().resize(self.window_normal_size)
-        if width > self.window_normal_size.width():
-            self._fix_x_delta_after_resize()
-
-    def _move_via_gesture(self):
-        """
-        window moving implementation
-        """
-
-        # if user wants to resize window with screen edge gesture
-        if self._shadow.isVisible():
-            self._use_shadow_geometry()
-            return
-
-        # straight moving
-        self._move_normal()
-        if self._is_gestured:
-            self._restore_normal_size()
-            self._is_gestured = False
-
-    def mouseReleaseEvent(self, a0: QtGui.QMouseEvent) -> None:
-        # saves event info
-        self._release_event = EventParser(self, a0)
-        # launches custom window moving implementation
-        if self._is_pressed:
-            self._move_via_gesture()
-        # drop defaults
-        self._is_pressed = False
-        self.window().setCursor(QtCore.Qt.CursorShape.ArrowCursor)
-        return super().mouseReleaseEvent(a0)
+from . import modes
 
 
 class CWindow(QtWidgets.QMainWindow):
@@ -174,10 +42,18 @@ class CWindow(QtWidgets.QMainWindow):
     titlebar_height = 44
     shadow_color = QtGui.QColor(0, 0, 0, 100)
 
-    title_bar: TitleBar
+    title_bar: QtWidgets.QFrame
     content: QtWidgets.QFrame
 
-    def __init__(self, parent = None):
+    gesture_mode: modes.GestureResizeMode = modes.GestureResizeModes.shrink_as_possible
+    gesture_sides: modes.SidesUsingMode = modes.SideUsingModes.whole
+    gesture_orientation_mode: modes.ScreenOrientationModes \
+        = modes.ScreenOrientationModes.no_difference
+
+    _press_event: EventParser
+    _release_event: EventParser
+
+    def __init__(self, parent=None):
 
         QtWidgets.QMainWindow.__init__(self, parent)
         # FramelessWindowHint makes window lose gesture functionality
@@ -191,6 +67,13 @@ class CWindow(QtWidgets.QMainWindow):
 
         self.title_bar = TitleBar(self, self.titlebar_height)
         layout.addWidget(self.title_bar)
+
+        self._is_pressed = False
+        # _is_gestured indicates that window was resized with moving to screen edge gesture
+        self._is_gestured = False
+        self._shadow = WindowShadow(self.shadow_color)
+        self._normal_size = self.size()
+        self._screen = ScreenParser(self.screen())
 
         self.content = QtWidgets.QFrame(self)
         self.content.setSizePolicy(
@@ -287,3 +170,139 @@ class CWindow(QtWidgets.QMainWindow):
     def moveEvent(self, a0: QtGui.QMoveEvent) -> None:
         super().moveEvent(a0)
         self._update_cw_geometry()
+
+    def _use_shadow_geometry(self):
+
+        """hides shadow and sets it's geometry to itself"""
+
+        if not self._is_gestured:
+            # saves window old size to restore it later
+            self._normal_size = self.window().size()
+        self._shadow.hide()
+        self._is_gestured = True
+        self.setGeometry(self._shadow.geometry())
+
+    def _move_normal(self):
+
+        """common way to move the window"""
+
+        # calculates delta between pressed and released points
+        pos1 = self._release_event.point
+        pos0 = self._press_event.point
+        dx = pos1.x() - pos0.x()
+        dy = pos1.y() - pos0.y()
+        posW = self.window().pos()
+        self.move(dx + posW.x(), dy + posW.y())
+
+    def _fix_x_delta_after_resize(self):
+
+        """
+        makes screen edge gesture exiting more intuitive
+        """
+        # window position after moving and resizing
+        pos = self.pos()
+
+        # if window was moved right
+        if self._release_event.point.x() > self._press_event.point.x():
+            x = self._press_event.event.pos().x()
+            # delta x = window width * relative cursor position when it was pressed
+            x -= int(self.width() * self._press_event.relative_pos[0])
+            pos.setX(x)
+
+        # if window was moved left
+        else:
+            # up to this point, the _release_event characteristics
+            # have been computed relative to the old window geometry.
+            # Updates them
+            self._release_event.parse_event()
+            # rdx = relative delta x = difference between
+            # cursor relative position to the old window geometry and the new one
+            rdx = abs(self._press_event.relative_pos[0] - self._release_event.relative_pos[0])
+            # delta x = window width * rdx
+            dx = int(self.width()*rdx)
+            pos.setX(pos.x() + dx)
+
+        self.move(pos)
+
+    def _restore_normal_size(self):
+        """
+        Restores normal window size after gestures
+        """
+        width = self.width()
+        self.resize(self._normal_size)
+        if width > self._normal_size.width():
+            self._fix_x_delta_after_resize()
+
+    def _move_via_gesture(self):
+        """
+        window moving implementation
+        """
+
+        # if user wants to resize window with screen edge gesture
+        if self._shadow.isVisible():
+            self._use_shadow_geometry()
+            return
+
+        # straight moving
+        self._move_normal()
+        if self._is_gestured:
+            self._restore_normal_size()
+            self._is_gestured = False
+
+    def _titlebar_mouse_pressed(self, a0: QtGui.QMouseEvent) -> None:
+        self._is_pressed = True
+        self._press_event = EventParser(self, a0)
+        self.setCursor(QtCore.Qt.CursorShape.ClosedHandCursor)
+
+    def _titlebar_mouse_moved(self, a0: QtGui.QMouseEvent) -> None:
+        if self._is_pressed:
+            # if pressed, checks that user moved window to the screen edge 
+            # and shows the shadow to indicate target window geometry
+            parser = EventParser(self, a0)
+            if parser.side and parser.shadow_rect:
+                self._shadow.show_(parser.shadow_rect)
+            else:
+                self._shadow.hide()
+
+    def _titlebar_mouse_released(self, a0: QtGui.QMouseEvent) -> None:
+        # saves event info
+        self._release_event = EventParser(self, a0)
+        # launches custom window moving implementation
+        if self._is_pressed:
+            self._move_via_gesture()
+        # drop defaults
+        self._is_pressed = False
+        self.setCursor(QtCore.Qt.CursorShape.ArrowCursor)
+
+
+class TitleBar(QtWidgets.QFrame):
+
+    """
+    Custom window title bar. Provides moving for frameless window
+    """
+
+    def __init__(
+            self,
+            cwindow: CWindow,
+            height: int):
+
+        QtWidgets.QFrame.__init__(self, cwindow)
+        self.setFixedHeight(height)
+        self.setSizePolicy(
+            QtWidgets.QSizePolicy(
+                QtWidgets.QSizePolicy.Policy.Expanding,
+                QtWidgets.QSizePolicy.Policy.Fixed)
+            )
+        self.cwindow = cwindow
+
+    def mousePressEvent(self, a0: QtGui.QMouseEvent) -> None:
+        self.cwindow._titlebar_mouse_pressed(a0)
+        return super().mousePressEvent(a0)
+
+    def mouseMoveEvent(self, a0: QtGui.QMouseEvent) -> None:
+        self.cwindow._titlebar_mouse_moved(a0)
+        return super().mouseMoveEvent(a0)
+
+    def mouseReleaseEvent(self, a0: QtGui.QMouseEvent) -> None:
+        self.cwindow._titlebar_mouse_released(a0)
+        return super().mouseReleaseEvent(a0)
